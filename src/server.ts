cc9573250ce2236ad8c1c4040dc257f7dccb4abb
temlabs <temilabinjo@hotmail.co.uk>
2022-05-23 23:00:40 +0100

    Better error reporting for get poll by id

--- src/server.ts
+++ src/server.ts
@@ -94,7 +94,11 @@ app.get<{ pollId: string; masterKey: string }>(
         ? res.status(404).json(retrievedPoll)
         : res.status(200).json(retrievedPoll)
     } catch (error) {
-      res.status(500).json(error)
+      if (error instanceof Error) {
+        res.status(500).json(error.message)
+      } else {
+        res.status(500).json(String(error))
+      }
     }
   }
 );
